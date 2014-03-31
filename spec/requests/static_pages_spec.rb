f4f15503dd7650d22d47c510454c4a61ffb62812
JinLee <jinlee0908@gmail.com>
2014-03-31 14:35:20 -0700

    modify rspec to include routing tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,97 +4,60 @@ describe "StaticPages" do
 
   subject { page }
 
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
+  end
+
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    expect(page).to have_title(full_title('About Us'))
+    click_link "Help"
+    expect(page).to have_title(full_title('Help'))
+    click_link "Contact"
+    expect(page).to have_title(full_title('Contact'))
+    click_link "Home"
+    click_link "Sign up now!"
+    expect(page).to have_title(full_title('Sign up'))
+    click_link "sample_app"
+    expect(page).to have_title(full_title(''))
+  end
+
   describe "Home Page" do
     before { visit root_path }
 
-    it { should have_content('Sample App') }
-    it { should have_title(full_title('')) }
-    # it { should have_title('Ruby on Rails Tutorial Sample App') }
+    let(:heading)     { 'Sample App' }
+    let(:page_title)  { '' }
+
+    it_should_behave_like "all static pages"
     it { should_not have_title('| Home') }
   end
-  	
-    # it "should have the content 'Sample App'" do
-      # visit '/static_pages/home'
-      # visit root_path
-    #   expect(page).to have_content('Sample App')
-    # end
-
-    # it "should have the base title" do
-      # visit '/static_pages/home'
-      # visit root_path
-      # expect(page).to have_title('Ruby on Rails Tutorial Sample App')
-      # expect(page).to have_title('Home') alternate match okay
-    # end 
-
-    # it "should not have a custom page title" do
-      # visit '/static_pages/home'
-      # visit root_path
-    #   expect(page).not_to have_title('| Home')
-    # end
-  # end
 
   describe "Help page" do
     before { visit help_path }
 
-    it { should have_content('Help') }
-    it { should have_title(full_title('Help')) }
-    # it { should have_title ('Ruby on Rails Tutorial Sample App | Help') }
+    let(:heading)     { 'Help' }
+    let(:page_title)  { 'Help' }
+
+    it_should_behave_like "all static pages"
   end
-
-  #   it "should have the content 'Help' " do 
-  #     # visit '/static_pages/help'
-  #     # visit help_path
-  #     expect(page).to have_content('Help')
-  #   end
-
-  #   it "should have the Help title" do
-  #     # visit '/static_pages/help'
-  #     # visit help_path
-  #     expect(page).to have_title('Ruby on Rails Tutorial Sample App | Help')
-  #     # expect(page).to have_title('Help') alternate match okay
-  #   end
-  # end
 
   describe "About page" do
     before { visit about_path }
+    let(:heading)     { 'About Us' }
+    let(:page_title)  { 'About Us' }
 
-    it { should have_content('About Us') }
-    it { should have_title(full_title('About Us')) }
-    # it { should have_title('About Us') }
+    it_should_behave_like "all static pages"
   end
-
-  #   it "should have the content 'About Us'" do
-  #     # visit '/static_pages/about'
-  #     # visit about_path
-  #     expect(page).to have_content('About Us')
-  #   end
-
-  #   it "should have the About Us title" do
-  #    # visit '/static_pages/about'
-  #    # visit about_path
-  #    # expect(page).to have_title('Ruby on Rails Tutorial Sample App | About Us')
-  #    expect(page).to have_title('About Us')
-  #   end
-  # end
 
   describe "Contact page"do
     before { visit contact_path }
+    let(:heading)     { 'Contact' }
+    let(:page_title)  { 'Contact' }
 
-    it { should have_content('Contact') }
-    it { should have_title(full_title('Contact')) }
-    # it { should have_title('Ruby on Rails Tutorial Sample App | Contact') }
+    it_should_behave_like "all static pages"
   end
 
-  #   it "should have the content 'Contact'" do
-  #     # visit '/static_pages/contact'
-  #     # visit contact_path
-  #     expect(page).to have_content('Contact')
-  #   end
-
-  #   it "should have the Contact title" do 
-  #     # visit '/static_pages/contact'
-  #     # visit contact_path
-  #     expect(page).to have_title('Ruby on Rails Tutorial Sample App | Contact')
-  #   end
-  # end
+ 
 end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,14 +1,1 @@
-def full_title(page_title)
-  base_title = 'Ruby on Rails Tutorial Sample App'
-  if page_title.empty?
-    base_title
-  else
-    "#{base_title} | #{page_title}"
-  end
-end
-
-# Of course, this is essentially a duplicate of the helper in Listing 4.2, 
-# but having two independent methods allows us to catch any typos in the base 
-# title. This is dubious design, though, and a better (slightly more advanced) 
-# approach, which tests the original full_title helper directly, appears in the 
-# exercises (Section 5.6)
+include ApplicationHelper
