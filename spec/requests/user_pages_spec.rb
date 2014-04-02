21a0edded2576d2c85afbe83032f7c36561f52b7
JinLee <jinlee0908@gmail.com>
2014-04-02 15:48:50 -0700

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -52,8 +52,9 @@ describe "UserPages" do
         before { click_button submit }
         let(:user) { User.find_by(email: 'user@example.com') }
 
+        it { should have_link('Sign out') }
         it { should have_title(user.name) }
-        it { should have_selector('div.alert.alert-success', text: 'Welcome to the Sample App!') }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
       end
     end
   end
