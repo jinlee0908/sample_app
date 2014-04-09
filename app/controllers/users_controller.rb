f2adac87731dd062d06ffd28bd716b27ccb2f573
JinLee <jinlee0908@gmail.com>
2014-04-09 10:52:27 -0700

    Add user microposts;

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def index
@@ -56,12 +57,12 @@ class UsersController < ApplicationController
 
   # Associated with before filters(actions)
 
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: 'Please sign in.' 
-    end
-  end
+  # def signed_in_user
+  #   unless signed_in?
+  #     store_location
+  #     redirect_to signin_url, notice: 'Please sign in.' 
+  #   end
+  # end
 
   def correct_user
     @user = User.find(params[:id])
